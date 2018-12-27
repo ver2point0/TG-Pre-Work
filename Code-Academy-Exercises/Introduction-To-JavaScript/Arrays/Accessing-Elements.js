37288aefadf26e6efa132184d729984683af382e
John McIntosh II <jmci10@hotmail.com>
2018-12-27 07:41:53 -0500

    Accessing elements

--- Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Accessing-Elements.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Accessing-Elements.js
@@ -1,5 +1,11 @@
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0];
+
+console.log(listItem);
+console.log(famousSayings[2]);
+console.log(famousSayings[3]);
 
 
 
-
-// GOING forward: comment what you learn each lesson
+// I learned how to access elements.
