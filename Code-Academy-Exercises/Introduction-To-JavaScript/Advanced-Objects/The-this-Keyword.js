dc703d0e8456f4e910b56b8d60a2ecea1215d05a
John McIntosh II <jmci10@hotmail.com>
2018-12-27 18:23:11 -0500

    The this keyword

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/The-this-Keyword.js
@@ -0,0 +1,11 @@
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  provideInfo() {
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}.`
+  }
+};
+
+console.log(robot.provideInfo());
+
+// I learned about the this keyword.
