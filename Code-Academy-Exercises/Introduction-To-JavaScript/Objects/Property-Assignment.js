b79e54dc380a85f41cf552e523a777b5d93d2e3c
John McIntosh II <jmci10@hotmail.com>
2018-12-27 17:55:06 -0500

    Property assignments

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Property-Assignment.js
@@ -0,0 +1,13 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+// Write your code below
+spaceship.color = "glorious gold";
+spaceship.numEngines = 4;
+delete spaceship['Secret Mission'];
+
+// I learned about property assignments.
