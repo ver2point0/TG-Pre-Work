42a8027d6df51dafa59aa9122d9b0e0f912a7bf4
John McIntosh II <jmci10@hotmail.com>
2018-12-27 08:50:09 -0500

    The break keyword

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/The-break-keyword.js
@@ -0,0 +1,12 @@
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write you code below
+for (let rapperArrayIndex = 0; rapperArrayIndex < rapperArray.length; rapperArrayIndex++){
+  console.log(rapperArray[rapperArrayIndex]);
+  if (rapperArray[rapperArrayIndex] ===  'Notorious B.I.G.'){
+    break;
+  }
+}
+console.log("And if you don't know, now you know.");
+
+// I learned about the break keyword.
