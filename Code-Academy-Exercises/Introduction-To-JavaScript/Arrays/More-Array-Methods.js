d1256c8bcb248f497013b6a44571bc4a897f4f07
John McIntosh II <jmci10@hotmail.com>
2018-12-27 07:58:46 -0500

    More array methods

--- Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/More-Array-Methods.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/More-Array-Methods.js
@@ -1,5 +1,14 @@
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+groceryList.unshift('popcorn');
+
+console.log(groceryList.slice(1, 4));
+
+const pastaIndex = groceryList.indexOf('pasta');
+console.log(pastaIndex);
 
 
 
-
-// GOING forward: comment what you learn each lesson
+// I learned about some non-mutating methods.
