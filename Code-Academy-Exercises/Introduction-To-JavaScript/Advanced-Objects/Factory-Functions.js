874c220629d77bfc6dd415c2bac329d8706b3f22
John McIntosh II <jmci10@hotmail.com>
2018-12-27 18:28:29 -0500

    Factory functions

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Factory-Functions.js
@@ -0,0 +1,14 @@
+const robotFactory = (model, mobile) => {
+  return {
+    model : model,
+		mobile: mobile,
+		beep () {
+      console.log('Beep Boop');
+    }
+	};
+};
+
+const tinCan = robotFactory('P-500', true);
+tinCan.beep();
+
+// I learned about factory functions.
