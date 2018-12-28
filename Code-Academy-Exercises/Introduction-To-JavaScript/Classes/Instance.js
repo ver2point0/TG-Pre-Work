2c48ff289ee6e4bac9fb0fde6f6cf0a1d1de7150
John McIntosh II <jmci10@hotmail.com>
2018-12-28 01:48:12 -0500

    Instance

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Instance.js
@@ -0,0 +1,12 @@
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+// I learned how to create a class instance.
