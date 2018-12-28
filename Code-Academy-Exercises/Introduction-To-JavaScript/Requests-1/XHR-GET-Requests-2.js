35d86c3de360b438fb669e4ebc0817d5d3b427d4
John McIntosh II <jmci10@hotmail.com>
2018-12-28 08:25:09 -0500

    XHR get requests 2

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-1/XHR-GET-Requests-2.js
@@ -0,0 +1,13 @@
+const xhr = new XMLHttpRequest();
+const url = 'https://api-to-call.com/endpoint';
+xhr.responseType = 'json';
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === XMLHttpRequest.DONE){
+    return xhr.response;
+  }
+}
+
+xhr.open('GET', url);
+xhr.send();
+
+// reconstruct XHR GET request.
