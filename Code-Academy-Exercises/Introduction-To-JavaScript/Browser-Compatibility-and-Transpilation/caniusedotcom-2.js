05af28a272a1bc840c00e2348253d4b89a89b095
John McIntosh II <jmci10@hotmail.com>
2018-12-28 03:37:45 -0500

    Can I use dot com 2

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,7 @@
+// Set the variable below to a number
+let esFivePercentageSupport;
+esFivePercentageSupport = 95.66;
+
+// Set the variable below to a number
+let esSixTemplateLiterals;
+esSixTemplateLiterals = 88.61;
