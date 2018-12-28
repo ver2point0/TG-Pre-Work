bd284b6359bdd47502d0d39a86af672d05483ce6
John McIntosh II <jmci10@hotmail.com>
2018-12-28 08:06:57 -0500

    The async keyword

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Async-Await/The-Async-Keyword.js
@@ -0,0 +1,30 @@
+function withConstructor(num){
+  return new Promise((resolve, reject) => {
+    if (num === 0){
+      resolve('zero');
+    } else {
+      resolve('not zero');
+    }
+  })
+}
+
+withConstructor(0)
+  .then((resolveValue) => {
+  console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
+})
+
+// Write your code below:
+async function withAsync(num){
+  if (num === 0){
+      return 'zero';
+    } else {
+      return 'not zero';
+    }
+}
+
+withAsync(100)
+  .then((resolveValue) => {
+  console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
+})
+
+// use async keyword to write functions that handle asynchronous actions.
