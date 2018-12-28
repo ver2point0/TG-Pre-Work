3f50a883ad2fa2c0df55e3892c8eadac4befa398
John McIntosh II <jmci10@hotmail.com>
2018-12-28 08:57:07 -0500

    Async post requests 2

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-POST-Requests-2.js
@@ -0,0 +1,17 @@
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint', {
+      method: 'POST',
+      body: JSON.stringify({id: 200})
+    })
+    if(response.ok){
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+// piece together a POST request using async and wait.
