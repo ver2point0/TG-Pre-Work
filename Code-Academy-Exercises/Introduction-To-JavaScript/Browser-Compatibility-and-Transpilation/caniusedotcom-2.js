3e072c3eb694d20524d5ab2c8e5fbda45fbbb692
John McIntosh II <jmci10@hotmail.com>
2018-12-28 03:45:17 -0500

    Transpilate with Babel

--- Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -5,3 +5,5 @@ esFivePercentageSupport = 95.66;
 // Set the variable below to a number
 let esSixTemplateLiterals;
 esSixTemplateLiterals = 88.61;
+
+// I learned what percentage of browsers support ES5 and 6.
